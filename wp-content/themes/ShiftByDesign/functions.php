6e6b27348764a8af812975494ef15faf1d12d9ae
Jesus <jesussbayot@gmail.com>
2018-08-10 13:04:28 +1200

    upload style.scss

--- wp-content/themes/ShiftByDesign/functions.php
+++ wp-content/themes/ShiftByDesign/functions.php
@@ -7,6 +7,13 @@
  * @package WP_Bootstrap_4
  */
 
+
+define( 'THEME_URI', get_template_directory_uri() );
+define( 'THEME_PATH', get_template_directory() );
+define( 'THEME_IMAGE_URL', THEME_URI . '/images' );
+define( 'THEME_JS', THEME_URI . '/js' );
+define( 'THEME_CSS', THEME_URI . '/css' );
+
 if ( ! function_exists( 'wp_bootstrap_4_setup' ) ) :
 	function wp_bootstrap_4_setup() {
 
@@ -145,10 +152,13 @@ add_action( 'widgets_init', 'wp_bootstrap_4_widgets_init' );
  */
 function wp_bootstrap_4_scripts() {
 	wp_enqueue_style( 'open-iconic-bootstrap', get_template_directory_uri() . '/assets/css/open-iconic-bootstrap.css', array(), 'v4.0.0', 'all' );
-	wp_enqueue_style( 'bootstrap-4', get_template_directory_uri() . '/assets/css/bootstrap.css', array(), 'v4.0.0', 'all' );
+	//wp_enqueue_style( 'bootstrap-4', get_template_directory_uri() . '/assets/css/bootstrap.css', array(), 'v4.0.0', 'all' );
 	wp_enqueue_style( 'wp-bootstrap-4-style', get_stylesheet_uri(), array(), '1.0.2', 'all' );
 
-	wp_enqueue_script( 'bootstrap-4-js', get_template_directory_uri() . '/assets/js/bootstrap.js', array('jquery'), 'v4.0.0', true );
+    wp_enqueue_style( 'bootstrap-4', get_template_directory_uri() . '/assets/css/theme.css', array(), 'v4.0.0', 'all' );
+
+
+    wp_enqueue_script( 'bootstrap-4-js', get_template_directory_uri() . '/assets/js/bootstrap.js', array('jquery'), 'v4.0.0', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
